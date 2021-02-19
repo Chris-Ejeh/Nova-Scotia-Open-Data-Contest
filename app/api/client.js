883d871acc8903912362d39aca15849e96368bef
Chris Ejeh <77654414+Chris-Ejeh@users.noreply.github.com>
2021-02-19 00:21:50 -0400

    Update client.js

--- app/api/client.js
+++ app/api/client.js
@@ -1,8 +1,7 @@
 import { create } from 'apisauce';
 
 const apiClient = create({
-	//baseURL: 'http://192.168.2.80:19001/api',
-	baseURL: 'http://192.168.0.7:3000/api',
+	baseURL: 'http://'
 });
 //whenever we call the server the promise is always resolved
 //apisauce gives us standardized errors
